c0fdf34c6c7a35ec5fccac85f399ee043b479e39
santiago <unaj.santiago.ortiz@gmail.com>
2024-09-29 18:42:56 -0300

    configuraciones del cors

--- DoctorAppBackend/Data/ApplicationDbContext.cs
+++ DoctorAppBackend/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Models.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,7 @@ namespace Data
         {
                 
         }
+
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
