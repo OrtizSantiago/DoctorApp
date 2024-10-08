644bc4b9b8649c7f40b3a808fa48e4d30cba4c7b
santiago <unaj.santiago.ortiz@gmail.com>
2024-10-08 09:45:23 -0300

    commit del frontend

--- DoctorAppBackend/API/Controllers/UsuarioController.cs
+++ DoctorAppBackend/API/Controllers/UsuarioController.cs
@@ -1,22 +1,29 @@
 ﻿using Data;
+using Data.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Models.DTOs;
 using Models.Entidades;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsuarioController : ControllerBase
+    
+    public class UsuarioController : BaseApiController
     {
         private readonly ApplicationDbContext _db;
+        private readonly ITokenServicio _tokenServicio;
 
-        public UsuarioController(ApplicationDbContext db)
+        public UsuarioController(ApplicationDbContext db, ITokenServicio tokenServivcio)
         {
             _db = db;
+            _tokenServicio = tokenServivcio;
         }
 
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
         {
@@ -24,11 +31,57 @@ namespace API.Controllers
             return Ok(usuarios);
         }
 
+        [Authorize]
         [HttpGet("{id}")] //   api/usuario/1
         public async Task<ActionResult<Usuario>> GetUsuario(int id)
         {
             var usuario = await _db.Usuarios.FindAsync(id);
             return Ok(usuario);
         }
+
+        [HttpPost("registro")]// POST: api/usuario/registro
+        public async Task<ActionResult<UsuarioDto>> Registro(RegistroDto registroDto)
+        {
+            if (await UsuarioExiste(registroDto.Username)) return BadRequest("UserName ya esta Registrado");
+
+            using var hmac = new HMACSHA512();
+            var usuario = new Usuario
+            {
+                UserName = registroDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registroDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _db.Usuarios.Add(usuario);
+            await _db.SaveChangesAsync();
+            return new UsuarioDto
+            {
+                UserName = usuario.UserName,
+                Token = _tokenServicio.CrearToken(usuario)
+            };
+        }
+        
+        [HttpPost("login")] // POST: api/usuario/login
+        public async Task<ActionResult<UsuarioDto>> login(LoginDto loginDto)
+        {
+            var usuario = await _db.Usuarios.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if (usuario == null) return Unauthorized("Usuario no valido");
+            using var hmac = new HMACSHA512(usuario.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != usuario.PasswordHash[i]) return Unauthorized("Password no valido");
+            }
+            return new UsuarioDto
+            {
+                UserName = usuario.UserName,
+                Token = _tokenServicio.CrearToken(usuario)
+            };
+        }
+
+        private async Task<bool> UsuarioExiste(string username)
+        {
+            return await _db.Usuarios.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
     }
 }
--- DoctorAppBackend/API/Program.cs
+++ DoctorAppBackend/API/Program.cs
@@ -1,21 +1,22 @@
+using API.Extensiones;
 using Data;
+using Data.Interfaces;
+using Data.Servicios;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddCors();
+builder.Services.AgregarServiciosAplicacion(builder.Configuration);
+builder.Services.AgregarServicioIdentidad(builder.Configuration);
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -27,6 +28,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors(x  => x.AllowAnyOrigin()
                    .AllowAnyHeader()
                    .AllowAnyMethod());
+app.UseAuthentication();
 
 app.UseAuthorization();
 
